a59e1279c16ad2b53098accd24fc6398181abab7
elghafiky <50608610+elghafiky@users.noreply.github.com>
2026-03-10 17:48:01 +0700

    Update 6 descriptive (paper).R

--- 6 descriptive (paper).R
+++ 6 descriptive (paper).R
@@ -7,7 +7,7 @@ graphics.off(); rm(list=ls());cat("\14");
 
 # Load packages
 #install.packages("pacman") # install the package if you haven't 
-pacman::p_load(rmarkdown, knitr, kableExtra, tidyverse)
+pacman::p_load(rmarkdown, knitr, kableExtra, tidyverse, kableExtra)
 
 # Retrieve the current system username
 current_user <- Sys.info()[["user"]]
@@ -29,7 +29,6 @@ master <- file.path(base_dir,
                     "9 Main survey")
 setwd(master)
 
-arc = file.path(getwd(), "1 archive")
 ipt = file.path(getwd(),"2a input")
 temp = file.path(getwd(),"2b temp")
 opt = file.path(getwd(),"2c output")
@@ -75,25 +74,113 @@ dfs <- map(filelist, function(files) {
 
 # Change outcome names
 rename_outcome <- function(x){
-
+  
   n <- as.numeric(str_extract(x, "\\d+$"))
-
+  
   case_when(
-    str_detect(x, "^CB05r_cloned") ~ paste0("CB05", letters[pmax(n - 2, 1)]),
-    str_detect(x, "^topthree") ~ paste0("CB06", letters[n]),
-    str_detect(x, "^agree") ~ paste0("CB07", letters[n]),
-    str_detect(x, "^bigrole") ~ paste0("CB11", letters[n]),
-    str_detect(x, "^support") ~ paste0("DK", n),
-    str_detect(x, "^higheff") ~ paste0("TD", n),
-
-    str_detect(x, "^CB06_Orderr_cloned") ~ paste0("CB06", letters[n]),
-    str_detect(x, "^CB07r_cloned") ~ paste0("CB07", letters[n]),
-    str_detect(x, "^CB11r_cloned") ~ paste0("CB11", letters[n]),
-    str_detect(x, "^QDKr_cloned") ~ paste0("DK", n),
-    str_detect(x, "^TD0_cloned") ~ paste0("TD", n),
-
+    str_detect(x, "CB05r_cloned")        ~ paste0("CB05", letters[n]),
+    str_detect(x, "topthree")            ~ paste0("CB06", letters[n]),
+    str_detect(x, "agree")               ~ paste0("CB07", letters[n]),
+    str_detect(x, "bigrole")             ~ paste0("CB11", letters[n]),
+    str_detect(x, "support|QDKr_cloned") ~ paste0("DK", n),
+    str_detect(x, "higheff|TD0_cloned")  ~ paste0("TD", n),
+    str_detect(x, "CB06_Orderr_cloned")  ~ paste0("CB06", letters[n]),
+    str_detect(x, "CB07r_cloned")        ~ paste0("CB07", letters[n]),
+    str_detect(x, "CB11r_cloned")        ~ paste0("CB11", letters[n]),
     TRUE ~ x
   )
 }
 dfs$binary$outcome <- rename_outcome(dfs$binary$outcome)
 dfs$likert$outcome <- rename_outcome(dfs$likert$outcome)
+
+# Import outcome label
+olables <- read_excel(file.path(ipt,"outcome label.xlsx"))
+
+# Merge labels
+dfs$binary <- dfs$binary |>
+  left_join(olables, by = c("outcome" = "Outcome"))
+dfs$likert <- dfs$likert |>
+  left_join(olables, by = c("outcome" = "Outcome"))
+
+# Prepare dataframe for table export
+prep_table <- function(df){
+  
+  df |>
+    arrange(Family) |>
+    select(
+      Family,
+      Outcome = Label,
+      Control_mean = mean_control,
+      T1 = T1_mean_difference,
+      T2 = T2_mean_difference,
+      T3 = T3_mean_difference,
+      T5 = T5_mean_difference
+    ) |>
+    rename(
+      "Control mean" = Control_mean,
+      "Fix the Distribution" = T1,
+      "No Victimization" = T2,
+      "Balanced Development" = T3,
+      "Equal Opportunity" = T5
+    ) |>
+    mutate(across(where(is.numeric), ~round(.x, 3)))
+}
+binary_tab <- prep_table(dfs$binary)
+likert_tab <- prep_table(dfs$likert)
+
+# Table rendering function
+make_outcome_table <- function(df, caption){
+  
+  family_sizes <- df |>
+    count(Family)
+  
+  tab_print <- df |>
+    select(-Family)
+  
+  tab <- kableExtra::kbl(
+    tab_print,
+    format = "latex",
+    booktabs = TRUE,
+    longtable = TRUE,
+    linesep = "",
+    caption = caption
+  ) |>
+    kableExtra::add_header_above(c(
+      " " = 2,
+      "Difference to control" = 4
+    )) |>
+    kableExtra::column_spec(1, width = "7cm") |>
+    kableExtra::kable_styling(
+      latex_options = "repeat_header"
+    )
+  
+  start <- 1
+  
+  for(i in seq_len(nrow(family_sizes))){
+    
+    end <- start + family_sizes$n[i] - 1
+    
+    tab <- tab |>
+      kableExtra::pack_rows(
+        family_sizes$Family[i],
+        start,
+        end,
+        indent = FALSE
+      )
+    
+    start <- end + 1
+  }
+  
+  tab
+}
+
+# Build the tables
+binary_table <- make_outcome_table(
+  binary_tab,
+  "Unadjusted differences of outcomes in binary indicator format"
+)
+
+likert_table <- make_outcome_table(
+  likert_tab,
+  "Unadjusted differences of outcomes in Likert format"
+)
